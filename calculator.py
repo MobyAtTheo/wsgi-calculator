07569658fd41541f0a6393cb3e7f93044d7d4ae3
Mark O'Brien <markob@uw.edu>
2018-07-26 16:51:54 -0700

    updated quoting style to pep8, removed debbuging notes and comments, marked items as done. 	modified:   calculator.py

--- calculator.py
+++ calculator.py
@@ -1,48 +1,48 @@
 #! /usr/bin/env python3
 
-# """
-# For your homework this week, you'll be creating a wsgi application of
-# your own.
-#
-# You'll create an online calculator that can perform several operations.
-#
-# You'll need to support:
-#
-#   * Addition
-#   * Subtractions
-#   * Multiplication
-#   * Division
-#
-# Your users should be able to send appropriate requests and get back
-# proper responses. For example, if I open a browser to your wsgi
-# application at 'http://localhost:8080/multiple/3/5' then the response
-# body in my browser should be '15'.
-#
-# Consider the following URL/Response body pairs as tests:
-#
-# '''
-#   http://localhost:8080/multiply/3/5   => 15
-#   http://localhost:8080/add/23/42      => 65
-#   http://localhost:8080/subtract/23/42 => -19
-#   http://localhost:8080/divide/22/11   => 2
-#   http://localhost:8080/               => <html>Here's how to use this page
-# ...</html>
-# '''
-#
-# To submit your homework:
-#
-#   * Fork this repository (Session03).
-#   * Edit this file to meet the homework requirements.
-#   * Your script should be runnable using '$ python calculator.py'
-#   * When the script is running, I should be able to view your
-#     application in my browser.
-#   * I should also be able to see a home page (http://localhost:8080/)
-#     that explains how to perform calculations.
-#   * Commit and push your changes to your fork.
-#   * Submit a link to your Session03 fork repository!
-#
-#
-# """
+"""
+For your homework this week, you'll be creating a wsgi application of
+your own.
+
+You'll create an online calculator that can perform several operations.
+
+You'll need to support:
+
+  * Addition
+  * Subtractions
+  * Multiplication
+  * Division
+
+Your users should be able to send appropriate requests and get back
+proper responses. For example, if I open a browser to your wsgi
+application at 'http://localhost:8080/multiple/3/5' then the response
+body in my browser should be '15'.
+
+Consider the following URL/Response body pairs as tests:
+
+'''
+  http://localhost:8080/multiply/3/5   => 15
+  http://localhost:8080/add/23/42      => 65
+  http://localhost:8080/subtract/23/42 => -19
+  http://localhost:8080/divide/22/11   => 2
+  http://localhost:8080/               => <html>Here's how to use this page
+...</html>
+'''
+
+To submit your homework:
+
+  * Fork this repository (Session03).
+  * Edit this file to meet the homework requirements.
+  * Your script should be runnable using '$ python calculator.py'
+  * When the script is running, I should be able to view your
+    application in my browser.
+  * I should also be able to see a home page (http://localhost:8080/)
+    that explains how to perform calculations.
+  * Commit and push your changes to your fork.
+  * Submit a link to your Session03 fork repository!
+
+
+"""
 import traceback
 import math
 
@@ -67,9 +67,6 @@ def multiply(*args):
 
     # DONE: Fill sum with the correct value, based on the
     # args provided.
-    # i = 0
-    # result = 0
-
     i = 1
     result = 1
     for i in args:
@@ -101,7 +98,7 @@ def subtract(*args):
     return str(int(result))
 
 
-# TODO: Add functions for handling more arithmetic operations.
+# DONE: Add functions for handling more arithmetic operations.
 
 
 def cos(*args):
