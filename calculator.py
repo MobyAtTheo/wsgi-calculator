482fc58c9785c62eca65e0627295ec2f170ede7a
Mark O'Brien <markob@uw.edu>
2018-07-25 20:47:24 -0700

    updated functions subtract / added trasing to NameError 	modified:   calculator.py

--- calculator.py
+++ calculator.py
@@ -1,46 +1,48 @@
 #! /usr/bin/env python3
-"""
-For your homework this week, you'll be creating a wsgi application of
-your own.
 
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at 'http://localhost:8080/multiple/3/5' then the response
-body in my browser should be '15'.
-
-Consider the following URL/Response body pairs as tests:
-
-'''
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-'''
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using '$ python calculator.py'
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
-"""
+# """
+# For your homework this week, you'll be creating a wsgi application of
+# your own.
+#
+# You'll create an online calculator that can perform several operations.
+#
+# You'll need to support:
+#
+#   * Addition
+#   * Subtractions
+#   * Multiplication
+#   * Division
+#
+# Your users should be able to send appropriate requests and get back
+# proper responses. For example, if I open a browser to your wsgi
+# application at 'http://localhost:8080/multiple/3/5' then the response
+# body in my browser should be '15'.
+#
+# Consider the following URL/Response body pairs as tests:
+#
+# '''
+#   http://localhost:8080/multiply/3/5   => 15
+#   http://localhost:8080/add/23/42      => 65
+#   http://localhost:8080/subtract/23/42 => -19
+#   http://localhost:8080/divide/22/11   => 2
+#   http://localhost:8080/               => <html>Here's how to use this page
+# ...</html>
+# '''
+#
+# To submit your homework:
+#
+#   * Fork this repository (Session03).
+#   * Edit this file to meet the homework requirements.
+#   * Your script should be runnable using '$ python calculator.py'
+#   * When the script is running, I should be able to view your
+#     application in my browser.
+#   * I should also be able to see a home page (http://localhost:8080/)
+#     that explains how to perform calculations.
+#   * Commit and push your changes to your fork.
+#   * Submit a link to your Session03 fork repository!
+#
+#
+# """
 import traceback
 
 
@@ -78,7 +80,7 @@ def divide(*args):
     pass
 
 
-def subtrace(*args):
+def subtract(*args):
     pass
 
 
@@ -98,14 +100,14 @@ def resolve_path(path):
 
     # path is something like add/3/5
     # or 'multiply/2/10/''
-    path = [1, 2, 3]
+    # path = [1, 2, 3]
 
     routes = {"add": add, "multiply": multiply, "subtract": subtract, "divide": divide}
 
     path = path.strip("/").split("/")
     func_name = path.pop(0)
-
     func = routes.get(func_name)
+
     # args = ["25", "32"]
     args = path
 
@@ -124,26 +126,26 @@ def application(environ, start_response):
     #         ...
     #      )
     # status = '200 OK'
-    def application(environ, start_response):
-        headers = [("Content-type", "text/html")]
-        try:
-            path = environ.get("PATH_INFO", None)
-            if path is None:
-                raise NameError
-            func, args = resolve_path(path)
-            body = func(*args)
-            status = "200 OK"
-        except NameError:
-            status = "404 Not Found"
-            body = "<h1>Not Found</h1>"
-        except Exception:
-            status = "500 Internal Server Error"
-            body = "<h1>Internal Server Error</h1>"
-            print(traceback.format_exc())
-        finally:
-            headers.append(("Content-length", str(len(body))))
-            start_response(status, headers)
-            return [body.encode("utf8")]
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get("PATH_INFO", None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+        print(traceback.format_exc())
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(("Content-length", str(len(body))))
+        start_response(status, headers)
+        return [body.encode("utf8")]
 
 
 if __name__ == "__main__":
